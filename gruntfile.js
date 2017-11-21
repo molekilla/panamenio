e7451a9f66c77e898352b853ac0df984ba3cda51
Rogelio Morrell <molekilla@gmail.com>
2017-11-21 11:40:08 -0500

    fixes

--- gruntfile.js
+++ gruntfile.js
@@ -133,19 +133,10 @@ module.exports = function(grunt) {
 
 
   grunt.loadNpmTasks('grunt-jasmine-node');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jasmine');
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-handlebars');
-  grunt.loadNpmTasks('grunt-nodemon');
-  grunt.loadNpmTasks('grunt-concurrent');
 
-  grunt.registerTask('dev', ['concurrent:target'])
   grunt.registerTask('spec', ['jasmine_node', 'jasmine', 'jshint'])
 
-  // Default task.
-  grunt.registerTask('default', [ 'handlebars', 'concat', 'uglify']);
 
 };
